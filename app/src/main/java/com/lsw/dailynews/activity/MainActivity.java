93d1f3cf5259f666137e4bdd1ad62d2395e8f6c6
liushuwei <liushuwei@qiyi.com>
2017-04-11 18:16:56 +0800

    修改menu的命名

--- app/src/main/java/com/lsw/dailynews/activity/MainActivity.java
+++ app/src/main/java/com/lsw/dailynews/activity/MainActivity.java
@@ -82,17 +82,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_news) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_photo) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_video) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_like) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_setting) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
