976dbf63b6966c1956dd34206c276407622d768c
liushuwei <liushuwei@qiyi.com>
2017-04-11 19:04:53 +0800

    增加启动页面

---
+++ app/src/main/java/com/lsw/dailynews/activity/SplashActivity.java
@@ -0,0 +1,103 @@
+package com.lsw.dailynews.activity;
+
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.lsw.dailynews.R;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @BindView(R.id.logo_outer_iv)
+    ImageView mLogoOuterIv;
+    @BindView(R.id.logo_inner_iv)
+    ImageView mLogoInnerIv;
+    boolean isShowingRubberEffect = false;
+    @BindView(R.id.app_name_tv)
+    TextView mAppNameTv;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.zoomin, 0);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        initAnimation();
+    }
+
+    private void initAnimation() {
+        startLogoInner1();
+        startLogoOuterAndAppName();
+    }
+
+    private void startLogoInner1() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim_top_in);
+        mLogoInnerIv.startAnimation(animation);
+    }
+
+    private void startLogoOuterAndAppName() {
+        final ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1);
+        valueAnimator.setDuration(1000);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float fraction = animation.getAnimatedFraction();
+                if (fraction >= 0.8 && !isShowingRubberEffect) {
+                    isShowingRubberEffect = true;
+                    startLogoOuter();
+                    startShowAppName();
+                    finishActivity();
+                } else if (fraction >= 0.95) {
+                    valueAnimator.cancel();
+                    startLogoInner2();
+                }
+
+            }
+        });
+        valueAnimator.start();
+    }
+
+    private void startLogoOuter() {
+        YoYo.with(Techniques.RubberBand).duration(1000).playOn(mLogoOuterIv);
+    }
+
+    private void startShowAppName() {
+        YoYo.with(Techniques.FadeIn).duration(1000).playOn(mAppNameTv);
+    }
+
+    private void startLogoInner2() {
+        YoYo.with(Techniques.Bounce).duration(1000).playOn(mLogoInnerIv);
+    }
+
+    private void finishActivity() {
+        Observable.timer(1000, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(@NonNull Long aLong) throws Exception {
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                        overridePendingTransition(0, android.R.anim.fade_out);
+                        finish();
+                    }
+                });
+    }
+}
